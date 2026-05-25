8286c1ea3dd7951dccc3b9e2b03fb2964a84098f
Dmitry <quicksolver11@gmail.com>
2026-05-25 23:04:00 +0200

    SEO: add JSON-LD BreadcrumbList schema to breadcrumbs template
    - Added structured data markup after existing nav HTML
    - Uses @once to prevent triple-rendering on catpage.blade.php
      (breadcrumbs included 3 times for different breakpoints)
    - Iterates over associative array [$key => $url] via array_keys/values
    - Applies strip_tags() to names for HTML safety
    - Uses url()->current() for active page items (where url = '')
    - Uses addslashes() to escape quotes in JSON

--- resources/views/includes/breadcrumbs.blade.php
+++ resources/views/includes/breadcrumbs.blade.php
@@ -18,3 +18,28 @@
         @endforeach
     </ol>
 </nav>
+@once
+@if(isset($b) && count($b) > 0)
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [
+    @php
+      $__bKeys = array_keys($b);
+      $__bVals = array_values($b);
+      $__bCount = count($b);
+    @endphp
+    @for($__idx = 0; $__idx < $__bCount; $__idx++)
+    {
+      "@type": "ListItem",
+      "position": {{ $__idx + 1 }},
+      "name": "{{ addslashes(strip_tags($__bKeys[$__idx])) }}",
+      "item": "{{ $__bVals[$__idx] !== '' ? $__bVals[$__idx] : url()->current() }}"
+    }{{ $__idx < $__bCount - 1 ? ',' : '' }}
+    @endfor
+  ]
+}
+</script>
+@endif
+@endonce
